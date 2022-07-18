d40b880feb7c1d0c71a7e60595baecb38d38a69e
satomi-haruna <satomi.haruna1124@gmail.com>
2022-07-18 01:08:41 +0000

    Finish ruby-6-2

--- ruby/lesson6-2.rb
+++ ruby/lesson6-2.rb
@@ -1,4 +1,26 @@
 puts "計算をはじめます"
 puts "何回繰り返しますか？"
 
-puts gets
+i = 1
+times = gets.to_i
+
+while i <= 5 do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "a+b=#{a+b}"
+  puts "a-b=#{a-b}"
+  puts "a*b=#{a*b}"
+  puts "a/b=#{a/b}"
+
+  if i == times
+    puts "処理を終了します"
+    break
+  end
+
+  i += 1
+end
