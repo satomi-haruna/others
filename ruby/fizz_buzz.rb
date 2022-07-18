02d711ce19f281440051536bdd713b9f53a219e7
satomi-haruna <satomi.haruna1124@gmail.com>
2022-07-18 07:03:50 +0000

    finish ruby-fizz

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -11,6 +11,6 @@ def fizz_buzz(num)
 end
 
 puts "数字を入力してください"
-num = gets.to_i
+num = gets.to_i #数字以外を想定していなくても念のため入れる模様
 puts "結果は…"
-puts fizz_buzz(num)
+puts fizz_buzz(num)　#メソッド名のみで呼び出せると思った、引数も必要
