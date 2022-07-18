b82fa677135934640421d8f48b9145b7b390a5bd
satomi-haruna <satomi.haruna1124@gmail.com>
2022-07-18 07:08:12 +0000

    update ruby-fizz

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -1,16 +1,16 @@
 def fizz_buzz(num)
   if (num %3 == 0)&&(num %5 == 0)
-    puts "FizzBuzz"
+    puts "FizzBuzz" #putsなしで良いみたい
   elsif num %3 == 0
-    puts "Fizz"
+    puts "Fizz" #putsなしで良いみたい
   elsif num %5 == 0
-    puts "Buzz"
+    puts "Buzz" #putsなしで良いみたい
   else
     num.to_s
   end
 end
 
 puts "数字を入力してください"
-num = gets.to_i #数字以外を想定していなくても念のため入れる模様
+num = gets.to_i #定義のnumとは異なる変数を指定が良い　input
 puts "結果は…"
-puts fizz_buzz(num)　#メソッド名のみで呼び出せると思った、引数も必要
+puts fizz_buzz(num)　#上記を反映して、fizz_buzz(input)
