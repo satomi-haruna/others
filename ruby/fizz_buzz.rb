94010d8df8749b117fa53a9bc67f13ff3028d4cb
satomi-haruna <satomi.haruna1124@gmail.com>
2022-07-18 06:52:51 +0000

    update ruby-fizz

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -1,4 +1,16 @@
-def fizz_buzz()
-
+def fizz_buzz(num)
+  if (num %3 == 0)&&(num %5 == 0)
+    puts "FizzBuzz"
+  elsif num %3 == 0
+    puts "Fizz"
+  elsif num %5 == 0
+    puts "Buzz"
+  else
+    num.to_s
+  end
 end
 
+puts "数字を入力してください"
+num = gets
+puts "結果は…"
+puts fizz_buzz
