cfe01e529a85ca11c00c1200b561efa10ffd6a45
satomi-haruna <satomi.haruna1124@gmail.com>
2022-08-07 06:58:06 +0000

    [Finish] ruby-6-2

--- ruby/lesson6-2.rb
+++ ruby/lesson6-2.rb
@@ -1,15 +1,17 @@
 puts "計算をはじめます"
-puts "何回繰り返しますか？"
+puts "何回計算を繰り返しますか？"
 
-i = 1
-times = gets.to_i
+num = gets.to_i
 
-#解答
-while i <= 5 do　#while i <= times do
+# i = 1の指定がいるかも？
+
+for i in 1..num do
   puts "#{i}回目の計算"
   puts "2つの値を入力してください"
+
   a = gets.to_i
   b = gets.to_i
+
   puts "a=#{a}"
   puts "b=#{b}"
   puts "計算結果を出力します"
@@ -18,12 +20,10 @@ while i <= 5 do　#while i <= times do
   puts "a*b=#{a*b}"
   puts "a/b=#{a/b}"
 
-  if i == times #不要
-    puts "処理を終了します"　#不要
-    break　#不要
-  end　#不要
-
-  i += 1
+  if i==num
+    puts "計算を終了します"
+    break
+  end
 end
 
-#繰り返し処理が終了してから、puts "計算を終了します"
+# 解答より：繰り返しが終了したあとにputs "計算を終了します"　を入れればそれでもＯＫ
