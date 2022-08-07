ec76f369330a83d5856aa2af5e776919d74373d8
satomi-haruna <satomi.haruna1124@gmail.com>
2022-08-07 06:35:21 +0000

    [Add] lesson4~6

--- ruby/lesson6-1.rb
+++ ruby/lesson6-1.rb
@@ -1,18 +1,6 @@
 puts "計算をはじめます"
 puts "2つの値を入力してください"
 
-a = gets
-b = gets
-
-puts "計算結果を出力します"
-product=a.to_i*b.to_i
-puts "a*b="+product.to_s
-puts "計算を終了します"
-
-
-puts "計算結果を出力します"
-puts "2つの値を入力してください"
-
 a = gets.to_i
 b = gets.to_i
 
